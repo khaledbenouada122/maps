963cb9723dafd8ea7cf3a78ec84b822307f89067
khaledbenouada122 <khaled.benouada@polytechnien.tn>
2022-10-21 12:02:49 +0100

    mahdia in maps

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Mapes from './mapes';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Mapes/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
